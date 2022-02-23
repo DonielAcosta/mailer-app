8b156176c4d4cebf2b280d682a63bc531ed54fe6
donielacosta1995 <donielacosta1995@gmailcom>
2022-02-22 22:38:24 -0400

    creacion de controladores, modelos, seeder factory

--- database/migrations/2022_02_23_013620_create_user_data_table.php
+++ database/migrations/2022_02_23_013620_create_user_data_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserDataTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_data', function (Blueprint $table) {
             $table->id();
-            $table->string('Identificador');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('password_verified');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('name',100);
+            $table->string('phone',10);
+            $table->string('identification',11);
+            $table->date('date_of_birth');
+            $table->string('code_city');
+
         });
     }
 
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_data');
     }
 }
